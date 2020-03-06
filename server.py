9a8072e8ef8cd3651f0f04d98371c7b665fee3f5
AaronWard <award40@LAMU02W63VHHTD6.uhc.com>
2020-03-06 15:27:44 +0000

    ADD - set root as static_url_path

--- server.py
+++ server.py
@@ -6,21 +6,29 @@ generation of up to date reports for the readme
 
 import os 
 import sys
-from flask import (Flask,send_file, request, g, redirect, url_for, abort, Response, jsonify, send_from_directory)
-from datetime import datetime, date, time 
+from flask import (Flask, send_file, request, jsonify, send_from_directory)
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(),'static')
 
-def list_paths(startpath='/'):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('{}{}'.format(subindent, f))
+# Make directory if it does not already exist
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 
 # function for running covidify
+def run_covidify():
+    os.system('cd ' + app.config['UPLOAD_FOLDER'] + ' && covidify run --output=./')
+    os.system('rm -rf ' + os.path.join(app.config['UPLOAD_FOLDER'], 'data'))
+    os.system('rm -rf ' + os.path.join(app.config['UPLOAD_FOLDER'], 'reports', '*.xlsx'))
+
+@app.route('/<path:filename>')
+def send_img(filename):
+    try:
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    except:
+        return jsonify({'unable to get image'})
+
+
 # Load the model and run the server
 if __name__ == "__main__":
     # app.debug = True
