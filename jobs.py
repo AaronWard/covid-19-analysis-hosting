46eb45c447c15928183762f05659ada84f7b6d61
AaronWard <award40@LAMU02W63VHHTD6.uhc.com>
2020-03-06 13:06:42 +0000

    uPDATE - works locally, testing on heroku

--- jobs.py
+++ jobs.py
@@ -7,7 +7,7 @@ sched = BlockingScheduler()
 '''
 This is will the function in app.py that dow
 '''
-@sched.scheduled_job('cron', day_of_week='0-6', hour='0-23', minute='0-59/3', timezone='America/New_York')
+@sched.scheduled_job('cron', day_of_week='0-6', hour='0-23', minute='0-59/2', timezone='America/New_York')
 def run_example():
     run_covidify()
 
--- server.py
+++ server.py
@@ -7,37 +7,33 @@ generation of up to date reports for the readme
 import os 
 import sys
 from flask import (Flask, request, g, redirect, url_for, abort, Response, jsonify, send_from_directory)
-from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime, date, time 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-sched = BlockingScheduler()
-app = Flask(__name__, static_folder='static', static_url_path='static')
-app.config['FOLDER'] = 'static'
+# sched = BlockingScheduler()
+app = Flask(__name__, static_folder='static')
+app.config['FOLDER'] = './static'
 app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'output')
 
-if not os.path.exists(app.config['UPLOADED_PHOTOS_DEST']):
-    os.makedirs(app.config['UPLOADED_PHOTOS_DEST'])
+if not os.path.exists(app.config['FOLDER']):
+    os.makedirs(app.config['FOLDER'])
 
 # called by the scheduler every hour
 def run_covidify():
     print('Running covidify at :', datetime.date(datetime.now()))
-    os.system('cd ' + app.config['FOLDER'])
-    os.system('covidify run --output=./')
-    # os.system('mv ./reports/images/*.jpg ./static')
-    # os.system('rm -rf ' + app.config['UPLOADED_PHOTOS_DEST'] + '/data')
-    # os.system('rm -rf ' + app.config['UPLOADED_PHOTOS_DEST'] + '/reports/*.xlsx')
+    os.system('cd ' + app.config['FOLDER'] + ' && covidify run --output=./')
+    os.system('rm -rf ' + os.path.join(app.config['FOLDER'], 'data'))
+    os.system('rm -rf ' + os.path.join(app.config['FOLDER'], 'reports', '*.xlsx'))
 
-
-@app.route('/images/<filename>') 
+@app.route('/<path:filename>') 
 def send_file(filename): 
     '''
     Return the requested images back to the github readme
     '''
     print(os.system('ls ' + str(os.getcwd())))
     print(os.system('ls ' + str(app.config['FOLDER'])))
-    return os.path.join(app.config['FOLDER'],'reports', 'images', filename)
+    return (app.config['FOLDER'],'reports', 'images', filename)
 
 @app.route('/') 
 def index(): 
