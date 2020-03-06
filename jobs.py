e15e92df2168ed138d1b763a874ec7d3b7d967a2
AaronWard <award40@LAMU02W63VHHTD6.uhc.com>
2020-03-06 14:17:58 +0000

    RESTART

--- jobs.py
+++ jobs.py
@@ -1,14 +1,15 @@
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-from server import run_covidify
+from server import list_paths # run_covidify
 
 sched = BlockingScheduler()
 
 '''
 This is will the function in app.py that dow
 '''
-@sched.scheduled_job('cron', day_of_week='0-6', hour='0-23', minute='0-59/2', timezone='America/New_York')
+@sched.scheduled_job('cron', day_of_week='0-6', hour='0-23', minute='0-59/1', timezone='America/New_York')
 def run_example():
-    run_covidify()
+    # run_covidify()
+    list_paths()
 
 sched.start()
--- server.py
+++ server.py
@@ -9,45 +9,18 @@ import sys
 from flask import (Flask,send_file, request, g, redirect, url_for, abort, Response, jsonify, send_from_directory)
 from datetime import datetime, date, time 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
 
-# sched = BlockingScheduler()
-app = Flask(__name__, static_folder='static')
-app.config['FOLDER'] = os.path.join(os.getcwd(),'static')
+def list_paths(startpath='/'):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
 
-if not os.path.exists(app.config['FOLDER']):
-    os.makedirs(app.config['FOLDER'])
-
-# called by the scheduler every hour
-def run_covidify():
-    print('Running covidify at :', datetime.date(datetime.now()))
-    os.system('cd ' + app.config['FOLDER'] + ' && covidify run --output=./')
-    os.system('rm -rf ' + os.path.join(app.config['FOLDER'], 'data'))
-    os.system('rm -rf ' + os.path.join(app.config['FOLDER'], 'reports', '*.xlsx'))
-
-@app.route('/<path:filename>') 
-def send_graph(filename): 
-    '''
-    Return the requested images back to the github readme
-    '''
-    print('FILE NAME: ', filename)
-
-    print('CWD ...')
-    print('...', os.system('ls ' + str(os.getcwd())))
-
-    print('FOLDER ...')
-    print('...', os.system('ls ' + str(app.config['FOLDER'])))
-
-    image = os.path.join(app.config['FOLDER'],'reports', 'images', filename)         
-    return send_file(image)
-
-@app.route('/') 
-def index(): 
-    '''
-    Return the requested images back to the github readme
-    '''
-    return jsonify({'results': 'example'})
-
+# function for running covidify
 # Load the model and run the server
 if __name__ == "__main__":
     # app.debug = True
